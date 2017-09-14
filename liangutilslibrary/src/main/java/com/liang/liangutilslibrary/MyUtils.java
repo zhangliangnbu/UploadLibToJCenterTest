e3d6eb48075bd66547f73f47599c0e8bbb009fcc
zhangliang <zhangliangnbu@163.com>
2017-09-14 11:40:54 +0800

    Initial commit

---
+++ liangutilslibrary/src/main/java/com/liang/liangutilslibrary/MyUtils.java
@@ -0,0 +1,12 @@
+package com.liang.liangutilslibrary;
+
+/**
+ *
+ * Created by zhangliang on 14/09/2017.
+ */
+
+public class MyUtils {
+	public static void print(String message) {
+		System.out.print("just test for:" + message);
+	}
+}
